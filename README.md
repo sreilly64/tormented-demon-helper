ad1ca0def5408674aa839ec99e48826e68d75f1e
Shane <sreilly@udel.edu>
2024-07-26 00:34:57 -0400

    Added config and updated readme

--- README.md
+++ README.md
@@ -4,14 +4,15 @@ This plugin adds quality of life improvements when fighting Tormented Demons (TD
 ## Features:
 
 ### Accuracy boost tracking
-- Roughly 25 ticks after either engaging in combat with a TD or after their fire bomb attack, the player's accurracy will be increased to 100% until the next fire bomb.
-- This plugin tracks this boost timer and displays it as a counter above the TD and/or as an icon on the TD denoting that the boost is active.
+- Roughly 30 ticks after either engaging in combat with a TD or after their fire bomb attack, the player's accuracy is increased to 100% until the next fire bomb.
+- This plugin tracks this boost timer and displays it as a counter on/above the TD (position is configurable).
+- The timer is color coded to distinguish between unboosted and boosted ticks (colors are configurable).
 
 ### TD Attack style highlighting
 - When fighting multiple TDs, it can be difficult to keep track of which attack style each one is using.
 - After a TD has performed an attack, this plugin can reactively highlight the TD with a color coded outline indicating which attack style it is currently using. 
 - The highlight colors can be configured, but the default values are:
-    - red = melee
-    - blue = mage
-    - green = range
-    - yellow = fire bomb
+    - melee = red 
+    - mage = blue
+    - range = green = range
+    - fire bomb = yellow
--- src/main/java/rr/helpers/tormenteddemonhelper/TormentedDemonHelperConfig.java
+++ src/main/java/rr/helpers/tormenteddemonhelper/TormentedDemonHelperConfig.java
@@ -1,19 +1,97 @@
 package rr.helpers.tormenteddemonhelper;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.*;
+import java.awt.*;
 
-@ConfigGroup("example")
+@ConfigGroup("tormenteddemonhelper")
 public interface TormentedDemonHelperConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+
+	@ConfigSection(
+			name = "Accuracy Boost Timer",
+			description = "Change the accuracy boost timer settings",
+			position = 1
 	)
-	default String greeting()
+	String AccuracyBoostTimerSettings = "Accuracy Boost Timer Settings";
+
+	@ConfigItem(
+			position = 1,
+			keyName = "showTimer",
+			name = "Show Accuracy Boost Timer",
+			description = "Toggles whether to overlay the number of ticks since start of combat or last fire bomb attack",
+			section = AccuracyBoostTimerSettings
+	)
+	default boolean showTimer()
 	{
-		return "Hello";
+		return true;
 	}
+
+	@ConfigItem(
+			position = 2,
+			keyName = "unboostedTickColor",
+			name = "Unboosted Tick Color",
+			description = "Configures the color of unboosted timer ticks",
+			section = AccuracyBoostTimerSettings
+	)
+	default Color unboostedTickColor()
+	{
+		return Color.RED;
+	}
+
+	@ConfigItem(
+			position = 3,
+			keyName = "boostedTickColor",
+			name = "Boosted Tick Color",
+			description = "Configures the color of boosted timer ticks",
+			section = AccuracyBoostTimerSettings
+	)
+	default Color boostedTickColor()
+	{
+		return Color.GREEN;
+	}
+
+	@ConfigItem(
+			position = 4,
+			keyName = "fontType",
+			name = "Timer Font Type",
+			description = "Changes the font of the boost timer overlay",
+			section = AccuracyBoostTimerSettings
+	)
+	default FontTypes fontType()
+	{
+		return FontTypes.REGULAR;
+	}
+
+	@Range(
+			min = 8,
+			max = 50
+	)
+	@ConfigItem(
+			position = 5,
+			keyName = "fontSize",
+			name = "Timer Font Size",
+			description = "Changes the font size of the boost timer overlay. Min = 8. Max = 50.",
+			section = AccuracyBoostTimerSettings
+	)
+	default int fontSize()
+	{
+		return 18;
+	}
+
+	@Range(
+			min = -70,
+			max = 10
+	)
+	@ConfigItem(
+			position = 6,
+			keyName = "verticalOffset",
+			name = "Timer Vertical Offset",
+			description = "Changes the vertical position of the timer overlay. Min = -70. Max = 10",
+			section = AccuracyBoostTimerSettings
+	)
+	default int verticalOffset()
+	{
+		return -30;
+	}
+
 }
